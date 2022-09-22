6b20e48e7f3971163ee2f46708b99fe2bbda9244
Matheus Henrique Luiz <matheus_henriluiz@hotmail.com>
2022-09-22 19:49:30 -0300

    att 22/09

--- Projeto Tabacaria/View/LoginScreen.Designer.cs
+++ Projeto Tabacaria/View/LoginScreen.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btEntrar = new Projeto_Tabacaria.Models.MNButton();
+            this.btSair = new Projeto_Tabacaria.Models.MNButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(78, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(78, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 237);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Projeto_Tabacaria.Properties.Resources.id_card;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Projeto_Tabacaria.Properties.Resources._lock;
+            this.pictureBox2.Location = new System.Drawing.Point(33, 233);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 27);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btEntrar
+            // 
+            this.btEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(204)))), ((int)(((byte)(55)))));
+            this.btEntrar.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(204)))), ((int)(((byte)(55)))));
+            this.btEntrar.BorderColor = System.Drawing.Color.Black;
+            this.btEntrar.BorderRadius = 10;
+            this.btEntrar.BorderSize = 0;
+            this.btEntrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btEntrar.FlatAppearance.BorderSize = 0;
+            this.btEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btEntrar.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btEntrar.ForeColor = System.Drawing.Color.Black;
+            this.btEntrar.Location = new System.Drawing.Point(111, 335);
+            this.btEntrar.Name = "btEntrar";
+            this.btEntrar.Size = new System.Drawing.Size(117, 33);
+            this.btEntrar.TabIndex = 8;
+            this.btEntrar.Text = "Entrar";
+            this.btEntrar.TextColor = System.Drawing.Color.Black;
+            this.btEntrar.UseVisualStyleBackColor = false;
+            // 
+            // btSair
+            // 
+            this.btSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(204)))), ((int)(((byte)(55)))));
+            this.btSair.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(204)))), ((int)(((byte)(55)))));
+            this.btSair.BorderColor = System.Drawing.Color.Black;
+            this.btSair.BorderRadius = 10;
+            this.btSair.BorderSize = 0;
+            this.btSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btSair.FlatAppearance.BorderSize = 0;
+            this.btSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btSair.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btSair.ForeColor = System.Drawing.Color.Black;
+            this.btSair.Location = new System.Drawing.Point(111, 384);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(117, 33);
+            this.btSair.TabIndex = 9;
+            this.btSair.Text = "Sair";
+            this.btSair.TextColor = System.Drawing.Color.Black;
+            this.btSair.UseVisualStyleBackColor = false;
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.ClientSize = new System.Drawing.Size(347, 472);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.btEntrar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Models.MNButton btEntrar;
+        private Models.MNButton btSair;
     }
 }
--- Projeto Tabacaria/View/LoginScreen.cs
+++ Projeto Tabacaria/View/LoginScreen.cs
@@ -1,10 +1,27 @@
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+
+
 namespace Projeto_Tabacaria
 {
     public partial class LoginScreen : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,    
+            int nRightRect,
+            int nBottomRect, 
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
         public LoginScreen()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
     }
 }
